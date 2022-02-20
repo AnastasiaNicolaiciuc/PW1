9ee95f801448848535c020ba1f24cecf757b2b60
Nastea <anastasianikolajcuk@gmail.com>
2022-02-20 21:10:31 +0300

    Ex. 1

---
+++ 2. 09-02-2022/ex1 (objects)/app.js
@@ -0,0 +1,26 @@
+/* Задача №1.
+Напишите код, выполнив задание из каждого пункта отдельной строкой:
+ */
+
+// ● Создайте пустой объект user.
+const user = {}
+console.log(user);
+
+// ● Добавьте свойство name со значением John.
+user.name = 'John';
+console.log(user.name);
+
+// ● Добавьте свойство surname со значением Smith.
+user.surname = 'Smith';
+console.log(user.surname);
+
+// ● Измените значение свойства name на Pete.
+user.name = 'Pete';
+console.log(user.name);
+
+// ● Удалите свойство name из объекта.
+delete user.name;
+console.log(user);
+
+
+
